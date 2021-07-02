cec96a026405abc2909fc4b9bb534c6e4d6ce19f
Cat Ormerod <catormerod@gmail.com>
2021-07-02 10:57:25 +1000

    Model and api Route created

---
+++ routes/api.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+//route to create new workout
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts/bulk", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//route to find all workouts
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//route to update exisitng workout
+router.get("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate({_id: req.params.id}, {workout: req.body})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
