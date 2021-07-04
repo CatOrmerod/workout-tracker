2b6b31a2909a7857be02bde7e34fcbe9b3482cf0
Cat Ormerod <catormerod@gmail.com>
2021-07-04 14:15:25 +1000

    remove dotenv

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const db = require("./models");
-require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
